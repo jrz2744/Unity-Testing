3b70723e0725e1cfe35ffb926e97455486836175
Jake Zoltun <jrz2744@rit.edu>
2024-01-25 17:16:07 -0500

    Built First WebGL

--- First3D/Assets/Scripts/PlayerMovement.cs
+++ First3D/Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
